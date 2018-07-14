bdb04686081968342203dd0523c585c9ea6fcb27
Dominik Ehrenberg <Dominik@Jungowski.de>
2018-07-14 14:10:36 +0200

    Buzzer: Render buzzer and show different buzzer image when hovering

--- src/compontents/Buzzer.js
+++ src/compontents/Buzzer.js
@@ -1,3 +1,42 @@
 /**
  * Created by Steff on 14/07/18.
  */
+import React from 'react';
+import BuzzerOff from '../assets/img/button-off.gif';
+import BuzzerOn from '../assets/img/button-on.gif';
+
+class Buzzer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            buzzer: BuzzerOff
+        }
+    }
+
+    onMouseOver() {
+        this.setState({
+            buzzer: BuzzerOn
+        });
+    }
+
+    onMouseOut() {
+        this.setState({
+            buzzer: BuzzerOff
+        });
+    }
+
+    render() {
+        return (
+            <img
+                src={this.state.buzzer}
+                className="Buzzer"
+                alt="Buzz!"
+                onMouseOver={() => this.onMouseOver()}
+                onMouseOut={() => this.onMouseOut()}
+            />
+        );
+    }
+}
+
+export default Buzzer;
