b8fef3f69de5010cf97da840bb44aa8d903fce8e
Dominik Ehrenberg <Dominik@Jungowski.de>
2018-07-14 15:55:04 +0200

    Websocket server that manages all connections

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const WebsocketServer = require('websocket').server;
+const http = require('http');
+const system = require('process');
+
+const port = process.env.PORT || 8080;
+const server = http.createServer();
+
+let wsConnections = [];
+
+var wsServer = new WebsocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+wsServer.on('request', request => {
+  const connection = request.accept();
+  console.log(`Connection from ${connection.remoteAddress} accepted`);
+  wsConnections.push(connection);
+
+  connection.on('message', message => {
+    console.log(message);
+  });
+
+  connection.on('close', () => {
+    const connectionIndex = wsConnections.findIndex(wsConnection => wsConnection === connection);
+    wsConnections = wsConnections.slice(connectionIndex, 1);
+    console.log(`Connection to ${connection.remoteAddress} closed`);
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on Port ${port}`);
+});
