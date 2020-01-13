9bb159680d0f108578a0a6e7dc59ac83d70ade07
Dominik Ehrenberg <doe@crosscan.com>
2020-01-13 14:14:25 +0100

    ws over ssl

--- src/config/websocket.js
+++ src/config/websocket.js
@@ -1,6 +1,6 @@
 const websocketConfig = {
   // address: 'ws://score11.de:8080'
-  address: 'ws://gamespodcast.de:8080'
+  address: "wss://www.gamespodcast.de/buzzer-socket"
   // address: 'ws://localhost:8080'
 };
 
