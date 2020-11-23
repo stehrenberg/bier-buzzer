a70aa192cefcea2db4d6dede14b4f9e5ea99e0d0
Dominik Ehrenberg <mail@dominik-ehrenberg.de>
2020-11-23 12:38:06 +0100

    Use live websocket

--- src/config/websocket.js
+++ src/config/websocket.js
@@ -1,7 +1,7 @@
 const websocketConfig = {
   // address: 'ws://score11.de:8080'
-  // address: "wss://www.gamespodcast.de/buzzer-socket"
-  address: 'ws://192.168.2.70:8080'
+  address: "wss://www.gamespodcast.de/buzzer-socket"
+  // address: 'ws://192.168.2.70:8080'
 };
 
 export default websocketConfig;
