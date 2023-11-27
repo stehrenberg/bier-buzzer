5c189c4c997ceeb37962093f42698eb2f59f32a5
Dominik Ehrenberg <mail@dominik-ehrenberg.de>
2023-11-27 08:52:25 +0100

    adds sounds for michael and ralf

--- src/config/users.js
+++ src/config/users.js
@@ -3,6 +3,8 @@ import SoundSebastian from '../assets/sound/Sebastian2020.mp3';
 import SoundJochen from '../assets/sound/FinishHimJochen.wav';
 import SoundDom from '../assets/sound/Dompotato_Buzzer.mp3';
 import SoundPaul from '../assets/sound/Paul_Quiz.mp3';
+import SoundMichael from '../assets/sound/michael_quiz_fin.mp3';
+import SoundRalf from '../assets/sound/ralf_quiz_fin.mp3';
 import roles from './roles.js';
 
 const users = [
@@ -35,6 +37,16 @@ const users = [
     name: "Paul",
     role: roles.ROLE_PLAYER,
     soundFile: SoundPaul
+  },
+  {
+    name: "Michael",
+    role: roles.ROLE_PLAYER,
+    soundFile: SoundMichael
+  },
+  {
+    name: "Ralf",
+    role: roles.ROLE_PLAYER,
+    soundFile: SoundRalf
   }
 ];
 
