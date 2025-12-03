4a477fa4d092062dae1c60fcd4b1cbd2c283a756
Dominik Ehrenberg <mail@dominik-ehrenberg.de>
2025-12-03 23:53:50 +0100

    feat: Adds sounds for Geraldine and JR

--- src/config/users.js
+++ src/config/users.js
@@ -1,6 +1,8 @@
 import SoundAndre from '../assets/sound/Andre2020.mp3';
+import SoundGeraldine from '../assets/sound/Geraldine_Buzzer_nc.mp3';
 import SoundSebastian from '../assets/sound/Sebastian2020.mp3';
 import SoundJochen from '../assets/sound/FinishHimJochen.wav';
+import SoundJochenR from '../assets/sound/JR_Buzzer_nc.mp3'
 import SoundDom from '../assets/sound/Dompotato_Buzzer.mp3';
 import SoundPaul from '../assets/sound/Paul_Quiz.mp3';
 import SoundMichael from '../assets/sound/michael_quiz_fin.mp3';
@@ -24,9 +26,19 @@ const users = [
     soundFile: SoundDom
   },
   {
-    name: "Jochen",
+    name: "Geraldine",
+    role: roles.ROLE_PLAYER,
+    soundFile: SoundGeraldine
+  },
+  {
+    name: "Jochen G.",
     role: roles.ROLE_PLAYER,
     soundFile: SoundJochen
+  },
+  {
+    name: "Jochen R.",
+    role: roles.ROLE_PLAYER,
+    soundFile: SoundJochenR
   },
   {
     name: "Sebastian",
